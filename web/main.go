73a25a81b767f25f8c0ff79cf1d962dc11d330fe
talia lopez <talialopezarias@gmail.com>
2025-04-25 22:47:07 +0200

    Incorporando datos dinámicos

--- web/main.go
+++ web/main.go
@@ -7,36 +7,80 @@ import (
 	"net/http"
 	//Vamos a registrar todo lo que esta ocurriendo dentro del servidor
 	"log"
+	//Se utiliza para trabajar con rutas de archivos de forma segura en diferentes SO
+	"html/template"
+	"path/filepath"
 )
 
+type PageData struct {
+	Title        string
+	Message      template.HTML
+	ErrorCode    int
+	ErrorMessage string
+}
+
+func renderTemplate(w http.ResponseWriter, tmplFile string, data PageData) {
+	tmpl, err := template.ParseFiles(tmplFile)
+
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	data := PageData{
+		Title:   "Mi primera página en Go",
+		Message: template.HTML("Es una página sencilla pero vamos aprendiendo <b>poco a poco</b> impartida por la programadora Talía López")
+	}
+
+	tmplFile := filepath.Join("web/templates/", "home.html")
+	renderTemplate(w, tmplFile, data)
+
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	data := PageData{
+		Title:        "Error 404",
+		ErrorCode:    404,
+		ErrorMessage: "¡Página no encontrada!",
+	}
+
+	tmplFile := filepath.Join("web/templates/", "error.html")
+	renderTemplate(w, tmplFile, data)
+
+}
+
 func main() {
-	// http.HandleFunc espera dos cosas:
-	// 1. Una ruta ("/") que será la URL que se escuche
-	// 2. Una función que se ejecuta cuando alguien accede a esa ruta.
+	//Creamos un manejador de archivos estáticos utilizando fileserver.
+	//http.Dir especifica en el directorio raiz donde se encuentran los archivos estaticos
+	//Handle es una ruta static para manejar las solicitud de archivos statics y luego utiliza StripPrefix eliminar el prefijo static.
+	fs := http.FileServer(http.Dir("web/statics"))
+	http.Handle("/statics/", http.StripPrefix("/statics/", fs))
+
 	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/products", productsHandler)
-	http.HandleFunc("/products/detail/", productDetailHandler)
+	http.HandleFunc("/error", errorHandler)
+
+	//Es una función controladora para la ruta raiz. La función utiliza el ServeFile y lo que hace es enviar el archivo home como respuesta al cliente.
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//http.ServeFile(w, r, filepath.Join("web/templates/", "home.html"))
+
+	//})
+
+	//http.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
+	//	http.ServeFile(w, r, filepath.Join("web/templates/", "error.html"))
+
+	//})
 
 	fmt.Println("Servidor está corriendo en http://localhost:8080")
 	//Primer argumento dirección IP y puerto, y el segundo para utilizar el manejador ya predeterminado.
 	// "nil" indica que usamos el manejador por defecto que ya configuramos con HandleFunc
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-// El primero se utiliza para enviar la respuesta al cliente y el Request representa la solicitud entrante.
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// Respondemos al navegador escribiendo directamente en el objeto "w"
-	// Esto se traduce en que el navegador verá "Hola mundo" en pantalla
-	fmt.Fprintln(w, "¡Bienvenido a la página de inicio con Go!")
-}
-
-func productsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Aquí se muestran todos los productos disponibles")
-}
-
-func productDetailHandler(w http.ResponseWriter, r *http.Request) {
-	//Se utiliza la función len para obtener la longitud de la ruta URL que se encuentra actual en la propiedad PATH
-	//del objeto r del tipo http.Rquest. Esta subcadena se asigna a productID de tipo String
-	productID := r.URL.Path[len("/products/detail/"):]
-	fmt.Fprintf(w, "Detalles del producto con ID: %s", productID)
 }
