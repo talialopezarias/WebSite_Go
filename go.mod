99d693a1e83cb23735d130e1155f8f3f89f81b1e
talia lopez <talialopezarias@gmail.com>
2025-04-25 18:23:47 +0200

    First commit using Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module WebSite_Go
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,13 @@
+// Declaramos que este archivo pertenece al paquete "main"
+// Es obligatorio para que Go sepa que aquí empieza el programa ejecutable
+package main
+
+// Importamos el paquete "fmt" que sirve para imprimir texto en la consola
+import "fmt"
+
+//Inicializamos una función. Esta es la función principal: el punto de entrada de cualquier programa en Go
+
+func main(){
+	// Usamos fmt.Println para imprimir "Hola mundo" en la consola
+	fmt.Println("Hola mundo")
+}
