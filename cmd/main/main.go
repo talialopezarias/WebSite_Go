c786552e29b825efcb0ad323adf22018e09b18cc
talia lopez <talialopezarias@gmail.com>
2025-05-02 21:08:22 +0200

    Definición de salida de mensajes de error

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,12 +6,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/talialopezarias/WebSite_Go/internal/config"
 	"github.com/talialopezarias/WebSite_Go/internal/routes"
 )
 
 func main() {
+	logFile, err := os.OpenFile("error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Error al abrir el archivo de registro: ", err)
+		return
+	}
+
+	defer logFile.Close() //aseguramos que el archivo se cierre correctamente cuando main se cierra
+
+	log.SetOutput(logFile)
+
 	cfg := config.LoadConfig()
 
 	routes.RegisterRoutes()
